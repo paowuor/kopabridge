fc8d5da74698fb7236497a49d7d378e23331eeaf
Paul Owuor <owuorpaul500@gmail.com>
2026-08-04 21:31:58 +0000

    feat(logging): implement LoggerMiddleware for HTTP request logging

---
+++ apps/api/src/common/utils/authorize-self-or-admin.ts
@@ -0,0 +1,33 @@
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger('HTTP');
+
+  use(req: Request, res: Response, next: NextFunction): void {
+    const requestId = (req.headers['x-request-id'] as string) || uuidv4();
+    req.headers['x-request-id'] = requestId;
+    res.setHeader('X-Request-ID', requestId);
+
+    const { method, originalUrl, ip } = req;
+    const start = Date.now();
+
+    res.on('finish', () => {
+      const { statusCode } = res;
+      const duration = Date.now() - start;
+      const logMessage = `[${requestId}]${method} ${originalUrl}${statusCode} - ${duration}ms - IP:${ip}`;
+
+      if (statusCode >= 500) {
+        this.logger.error(logMessage);
+      } else if (statusCode >= 400) {
+        this.logger.warn(logMessage);
+      } else {
+        this.logger.log(logMessage);
+      }
+    });
+
+    next();
+  }
+}
